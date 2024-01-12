edaa1c34e2fad5b76328f5269bb0f6c932e2ac4d
davidwiant <30698490+davidwiant@users.noreply.github.com>
2024-01-12 01:46:18 +0000

    adding route for jobpages

--- database.py
+++ database.py
@@ -19,4 +19,15 @@ def load_jobs_from_db():
     for row in result.all():
       jobs.append(row._asdict())
     return jobs
-  
+
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    query = text("SELECT * FROM jobs WHERE id = :val")
+    result = conn.execute(query, {'val': id})
+    rows = result.all()
+    
+    if len(rows) == 0:
+      return None
+    else:
+      return rows[0]._asdict()
+      
